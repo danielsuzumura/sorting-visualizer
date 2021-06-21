68dca36e3a380373ce578f07887fdf7adf1a9363
Daniel Suzumura <51420150+danielsuzumura@users.noreply.github.com>
2021-06-21 12:30:44 -0300

    Update sort.test.js

--- script/sort.test.js
+++ script/sort.test.js
@@ -4,27 +4,6 @@
 
 import * as sorts from "./sort.js";
 
-function createTestArray(n,array) {
-    "use strict";
-    let i;
-    if(array == null){
-        array = [];
-        for (i = 0; i < n; i += 1) {
-            array.push((Math.random() * 100) + 1);
-        }
-    }
-    const sizes = [];
-    for (i = 0; i < n; i += 1) {
-        let height = array[i];
-        let bar = document.createElement("div");
-        bar.style.height = height + "px";
-        bar.style.width = 100 + "px";
-        bar.classList.add("bar");
-        sizes.push(bar);
-    }
-    return sizes;
-}
-
 test('sort array', () => {
     let sizes = createTestArray(30);
     sorts.quicksort(sizes);
